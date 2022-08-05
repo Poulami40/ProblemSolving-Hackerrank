3d73d36497a2c5160a490ba5e3a8b2554aaa7156
Poulami Basu <98650837+Poulami40@users.noreply.github.com>
2022-08-05 19:40:30 +0530

    Create Plus Minus.cpp

---
+++ Plus Minus.cpp
@@ -0,0 +1,99 @@
+/*Print the ratios of positive, negative and zero values in the array. Each value should be printed on a separate line with 6 digits after the decimal. The function should not return a value.*/
+
+#include <bits/stdc++.h>
+#include <iomanip>
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'plusMinus' function below.
+ *
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+void plusMinus(vector<int> arr) {
+    int p=0,n=0,z=0,l=arr.size();
+    double rp,rn,rz;
+    for(int i=0;i<l;i++)
+    {
+        if(arr[i]>0)
+        p++;
+        else if(arr[i]<0)
+        n++;
+        else {
+        z++;
+        }
+    }
+    rp=(double)p/l;
+    rn=(double)n/l;
+    rz=(double)z/l;
+    cout<<setprecision(6);
+    cout<<rp<<endl<<rn<<endl<<rz<<endl;
+}
+
+int main()
+{
+    string n_temp;
+    getline(cin, n_temp);
+
+    int n = stoi(ltrim(rtrim(n_temp)));
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+    vector<int> arr(n);
+
+    for (int i = 0; i < n; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    plusMinus(arr);
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
